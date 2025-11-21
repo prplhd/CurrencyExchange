6c0057e7167ee9e136fd0d3a056686dc17f72028
prplhd <dimas.kovalenko2000@mail.ru>
2025-11-21 15:19:08 +0700

    make CurrencyMapper.java utility class

--- src/main/java/ru/prplhd/currencyexchange/mapper/CurrencyMapper.java
+++ src/main/java/ru/prplhd/currencyexchange/mapper/CurrencyMapper.java
@@ -3,8 +3,15 @@ package ru.prplhd.currencyexchange.mapper;
 import ru.prplhd.currencyexchange.dto.CurrencyDto;
 import ru.prplhd.currencyexchange.model.Currency;
 
-public class CurrencyMapper {
+public final class CurrencyMapper {
+    private CurrencyMapper() {}
+
     public static CurrencyDto toDto(Currency currency) {
-        return new CurrencyDto(currency.getId(), currency.getCode(), currency.getFullName(), currency.getSign());
+        return new CurrencyDto(
+                currency.getId(),
+                currency.getCode(),
+                currency.getFullName(),
+                currency.getSign()
+        );
     }
 }
