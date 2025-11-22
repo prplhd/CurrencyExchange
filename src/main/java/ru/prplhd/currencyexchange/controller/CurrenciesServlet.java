c8333677c50e047deb6b14fdf47775baa2d919c3
prplhd <dimas.kovalenko2000@mail.ru>
2025-11-22 18:36:58 +0700

    little fix CurrenciesServlet.java

--- src/main/java/ru/prplhd/currencyexchange/controller/CurrenciesServlet.java
+++ src/main/java/ru/prplhd/currencyexchange/controller/CurrenciesServlet.java
@@ -10,36 +10,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import ru.prplhd.currencyexchange.dao.CurrencyDao;
 import ru.prplhd.currencyexchange.dto.CurrencyDto;
 import ru.prplhd.currencyexchange.service.CurrencyService;
-
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/currencies")
 public class CurrenciesServlet extends HttpServlet {
-    Gson gson = new Gson();
-    CurrencyService currencyService;
+    private static final Gson GSON = new Gson();
+    private CurrencyService currencyService;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
+        super.init(config);
         CurrencyDao dao = new CurrencyDao();
-        this.currencyService = new CurrencyService(dao);
+        currencyService = new CurrencyService(dao);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json;charset=UTF-8");
         resp.setStatus(HttpServletResponse.SC_OK);
 
         List<CurrencyDto> currencyDtos = currencyService.getAllCurrencies();
-        String json = gson.toJson(currencyDtos);
-
-//        String json = """
-//            [
-//              {"code":"USD","name":"Доллар США"},
-//              {"code":"EUR","name":"Евро"}
-//            ]
-//            """;
+        String json = GSON.toJson(currencyDtos);
 
         resp.getWriter().write(json);
     }
