2028682dc3cf6f8eb3772a1cf87b6bb0b24bce4a
prplhd <dimas.kovalenko2000@mail.ru>
2025-12-06 20:52:43 +0700

    switch to HikariCP for connection pooling

--- src/main/java/ru/prplhd/currencyexchange/database/ConnectionProvider.java
+++ src/main/java/ru/prplhd/currencyexchange/database/ConnectionProvider.java
@@ -1,49 +1,22 @@
 package ru.prplhd.currencyexchange.database;
 
-import ru.prplhd.currencyexchange.exception.DataAccessException;
-import java.io.IOException;
-import java.io.InputStream;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public final class ConnectionProvider {
     private static final String PROPERTIES_FILE = "db.properties";
-    private static final String DRIVER_KEY = "db.driver";
-    private static final String URL_KEY = "db.url";
-    private static String url;
-    private static String initializationErrorMessage;
+    private static final HikariDataSource HIKARI_DATA_SOURCE;
 
     private ConnectionProvider() {}
 
     static {
-        try (InputStream inputStream = ConnectionProvider.class.getClassLoader()
-                .getResourceAsStream(PROPERTIES_FILE)) {
-
-            Properties properties = new Properties();
-            properties.load(inputStream);
-            String driver = properties.getProperty(DRIVER_KEY);
-            url = properties.getProperty(URL_KEY);
-
-            Class.forName(driver);
-        } catch (IOException e) {
-            initializationErrorMessage = "Failed to load database configuration from "
-                    + PROPERTIES_FILE;
-        } catch (ClassNotFoundException e) {
-            initializationErrorMessage = "JDBC driver class not found.";
-        }
+        HikariConfig config = new HikariConfig(PROPERTIES_FILE);
+        HIKARI_DATA_SOURCE = new HikariDataSource(config);
     }
 
-    public static Connection getConnection() {
-        if (initializationErrorMessage != null) {
-            throw new DataAccessException(initializationErrorMessage);
-        }
-
-        try {
-            return DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            throw new DataAccessException("Unable to connect to database.", e);
-        }
+    public static Connection getConnection() throws SQLException {
+        return HIKARI_DATA_SOURCE.getConnection();
     }
 }
